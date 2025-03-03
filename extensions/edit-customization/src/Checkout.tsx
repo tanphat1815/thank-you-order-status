ad53739c05bb12f8bd26800c737ca111693fd68e
tanphat1815 <tanphat1815@gmail.com>
2025-03-03 09:41:26 +0700

    test

--- extensions/edit-customization/src/Checkout.tsx
+++ extensions/edit-customization/src/Checkout.tsx
@@ -55,7 +55,7 @@ function Extension({ isOrderStatus }) {
                   fulfillments(first: 1) { 
                     edges { 
                       node { 
-                        status 
+                        status  
                       }
                     }
                   } 
